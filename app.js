f62608c85e111d727fef1985cf7aec30e48be8e2
JitenMehta01 <40121263+JitenMehta01@users.noreply.github.com>
2021-04-13 11:40:51 +0100

    added get/post request for cookies

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const app = express();
+const cookieParser = require('cookie-parser');
 
 app.use(express.urlencoded());// middleware
-
+app.use(cookieParser());
 
 app.set('view engine', 'pug')
 
@@ -15,11 +15,22 @@ app.get('/cards', (req, res) =>{
     res.render('card', {prompt: "Testing local var" , hint: "Think about who's tomb it is."});
 });
 
-app.all('/hello', (req, res) =>{
+app.post('/hello', (req, res) =>{
+    res.cookie('username', req.body.username)
     res.render('hello', {name: req.body.username});
-
 })
 
+app.get('/hello', (req, res) =>{
+    res.render('hello', {name: req.cookies.username});
+})
+
+// attempted to use .all method to render hello pug
+
+// app.all('/hello', (req, res) =>{
+//     res.cookie('username', req.body.username)
+//     res.render('hello', {name: req.body.username});
+//     res.render('hello', {name: req.cookies.username});
+// })
 
 app.listen('3000', () =>{
     console.log('This expresss app in running on local host')
